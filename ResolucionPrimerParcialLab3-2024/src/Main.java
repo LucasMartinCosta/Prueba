de6a9d695d3a7bfb611642b6578f70aab0e534d3
LucasMartinCosta <lucassmartin.costa@gmail.com>
2024-05-24 10:09:19 -0300

    prueba aventura

--- ResolucionPrimerParcialLab3-2024/src/Main.java
+++ ResolucionPrimerParcialLab3-2024/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
 
         ///AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//
+        ///AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//
+        ///AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA//
 
         // Creando agencia
         AgenciaViajes travelFan=new AgenciaViajes();
